95d447b5d27e8f7ce9d002f2fd2563628d5e911f
Melek <127142366+melekkrkmz@users.noreply.github.com>
2023-09-12 21:01:20 +0300

    Add files via upload

---
+++ HiddenSearchWidget/script.js
@@ -0,0 +1,10 @@
+
+const search=document.querySelector("i")
+const input=document.querySelector("input")
+
+search.onclick=()=>{
+
+    input.classList.toggle("active")
+    search.classList.toggle("active")
+
+}
