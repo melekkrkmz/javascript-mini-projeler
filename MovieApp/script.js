acf8ea1004afa5e35c083965cd1a484450435884
Melek <127142366+melekkrkmz@users.noreply.github.com>
2023-11-10 10:59:55 +0300

    Update script.js

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -1,8 +1,12 @@
 const containar = document.querySelector(".cart-containar")
+const search = document.getElementById("search")
+
+console.log(search)
 
 // istek at
 const apiCall = async () => {
 
+    // the movie database sitesi
     const request = await fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1")
     const data = await request.json()
     console.log(data)
@@ -24,18 +28,46 @@ const apiCall = async () => {
         <div class="explanation">
             <h2 style="margin-bottom: 5px;">${movie.title}</h2><hr>
             ${movie.overview}
-        </div>
-    
-   
-    `
+        </div>  `
         //Oluşturulan elemente bir key ve value tanımlarken setAttribute("key", "value"); fonksiyonunu bu şekilde kullanıyoruz.
         // bunu id vermek için kullandık
         movieDiv.setAttribute("id", movie.id)
         containar.appendChild(movieDiv)
 
+
+
     });
 
-}
+   
+    
+  let movies=document.querySelectorAll(".movie")
+  console.log("movie",movies)
 
+  search.addEventListener("input",()=>{
+
+    const searchValue=search.value.toLowerCase()
+    
+    // bütün movi divlerin üzerinde dön
+    movies.forEach(movieDiv => {
+        //  divlerin içindeki tittle ulaş ve textini küçült
+        const title = movieDiv.querySelector(".batton-containar h3").innerHTML.toLowerCase();
+        //   eğer tittleda search kımını içeriyor ise divin displayini block yap yok ise none yap
+        if (title.includes(searchValue)) {
+
+            movieDiv.style.display = "block";
+
+        } else {
+
+            movieDiv.style.display = "none";
+
+        }
+    })
+   
+
+  })  
+}
+ 
 
 apiCall()
+
+
